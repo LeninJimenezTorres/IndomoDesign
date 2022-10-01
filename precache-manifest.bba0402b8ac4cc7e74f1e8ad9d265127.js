2c3a6ca2b6590eb65ca761fee35d30c4bb210cda
Lenin Jiménez <jlenin25@hotmail.com>
2022-10-01 18:22:25 -0500

    Updates

---
+++ precache-manifest.bba0402b8ac4cc7e74f1e8ad9d265127.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "99fc9dff43eb0853d770",
+    "url": "/IndomoDesign/static/css/main.66a0dae9.chunk.css"
+  },
+  {
+    "revision": "99fc9dff43eb0853d770",
+    "url": "/IndomoDesign/static/js/main.99fc9dff.chunk.js"
+  },
+  {
+    "revision": "e4c123256ec82141fcb1",
+    "url": "/IndomoDesign/static/js/1.e4c12325.chunk.js"
+  },
+  {
+    "revision": "f021c859054d15e4d369",
+    "url": "/IndomoDesign/static/css/2.61809c29.chunk.css"
+  },
+  {
+    "revision": "f021c859054d15e4d369",
+    "url": "/IndomoDesign/static/js/2.f021c859.chunk.js"
+  },
+  {
+    "revision": "c7150f9b96db9a018a12",
+    "url": "/IndomoDesign/static/js/runtime~main.c7150f9b.js"
+  },
+  {
+    "revision": "691acbfdf7909367a97c4503be2d743b",
+    "url": "/IndomoDesign/static/media/icon-phone.691acbfd.png"
+  },
+  {
+    "revision": "4257d28894afe073c9414b815b029a54",
+    "url": "/IndomoDesign/static/media/Everlasting_Dream_-_Ancient_art_of_magic.4257d288.mp3"
+  },
+  {
+    "revision": "133a00819ed6d0bd297b5f235814b268",
+    "url": "/IndomoDesign/static/media/volume.133a0081.png"
+  },
+  {
+    "revision": "5ec6f9f6cc875165c2652992af84cf96",
+    "url": "/IndomoDesign/static/media/volume-mute.5ec6f9f6.png"
+  },
+  {
+    "revision": "499d50fd32cef9588cd6a61dab098172",
+    "url": "/IndomoDesign/static/media/arquitectura.499d50fd.mp4"
+  },
+  {
+    "revision": "90e72bfc8ec692fd274e34bd263c6ea7",
+    "url": "/IndomoDesign/static/media/interiorismo.90e72bfc.mp4"
+  },
+  {
+    "revision": "ef2ad4842e06f389858f4a7727df67a1",
+    "url": "/IndomoDesign/static/media/social-media.ef2ad484.gif"
+  },
+  {
+    "revision": "ef8e59223fd655415c2a0ef549154af4",
+    "url": "/IndomoDesign/static/media/chat-animation.ef8e5922.gif"
+  },
+  {
+    "revision": "47b149fefd0012704b06b08da09f2455",
+    "url": "/IndomoDesign/static/media/email.47b149fe.gif"
+  },
+  {
+    "revision": "aed1b307256b296b75663cf250deb596",
+    "url": "/IndomoDesign/static/media/developer.aed1b307.gif"
+  },
+  {
+    "revision": "964706aae3416a5424dab51dec38238b",
+    "url": "/IndomoDesign/static/media/cross.964706aa.png"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/IndomoDesign/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/IndomoDesign/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/IndomoDesign/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/IndomoDesign/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "96c2aabf1f250c855f8d42c03d9c9206",
+    "url": "/IndomoDesign/index.html"
+  }
+];
